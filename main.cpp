43ea5e9916946b6fa88e0c2a5503f93f4dad1ccb
yasha424 <jakob20022404@gmail.com>
2021-04-10 17:14:19 +0300

    copying image

--- main.cpp
+++ main.cpp
@@ -6,8 +6,14 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 
+  if (argc != 4) {
+    cout << "Incorrect input, for example use: \"./main bmp.bmp newbmp.bmp 3\"" << endl;
+    return 0;
+  }
+
   char* infile = argv[1];
   char* outfile = argv[2];
+  int change = stoi(argv[3]);
 
   FILE* inptr = fopen(infile, "r");
   FILE* outptr = fopen(outfile, "w");
@@ -15,6 +21,15 @@ int main(int argc, char* argv[]){
   BMPHEAD bh;
   fread(&bh, sizeof(BMPHEAD), 1, inptr);
 
-  cout << bh.height << endl << bh.width << endl;
+  fwrite(&bh, sizeof(BMPHEAD), 1, outptr);
+
+  for (size_t i = 0; i < bh.height; i++) {
+    for (size_t j = 0; j < bh.width; j++) {
+      PIXELDATA pixel;
+      fread(&pixel, sizeof(PIXELDATA), 1, inptr);
+
+      fwrite(&pixel, sizeof(PIXELDATA), 1, outptr);
+    }
+  }
 
 }
