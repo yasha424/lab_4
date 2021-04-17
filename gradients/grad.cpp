80d18aa119a1d18e2fa2c7c9a48991d62b74baba
yasha424 <jakob20022404@gmail.com>
2021-04-17 13:34:10 +0300

    managed folders

--- gradients/grad.cpp
+++ gradients/grad.cpp
@@ -3,24 +3,28 @@
 using namespace std;
 
 int main() {
-    const char* filename;  // rgb(0, 230, 255);
+    const char* filename;  // rgb(255, 0, 208);
     string name = "grad.bmp";
     // cin >> name;
     filename = name.data();
     FILE* outptr = fopen(filename, "w");
     BMPHEAD bh;
+    bh.biXPelsPerMeter = 72;
+    bh.biYPelsPerMeter = 72;
+
+
     fwrite(&bh, sizeof(BMPHEAD), 1, outptr);
     // PIXELDATA pixel;
     for (size_t i = 0; i < bh.height; i++) {
         PIXELDATA pixel(140, 255, 0);
         for (size_t j = 0; j < bh.width; j++) {
-            if (j % 32 == 0) {
-                pixel.red += -1;
+            if (j % 34 == 0) {
+                pixel.red += 1;
             }
-            if (j % 240 == 0) {
+            if (j % 16 == 0) {
                 pixel.green += -1;
             }
-            if (j % 20 == 0) {
+            if (j % 30 == 0) {
                 pixel.blue += 1;
             }
             fwrite(&pixel, sizeof(PIXELDATA), 1, outptr);
